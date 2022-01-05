e808acfd9b59caf33437e4e9dd9740c471f5e0e4
Gerald Addo-Tetteh <geraldadt@outlook.com>
2022-01-05 08:40:13 +0000

    add contact page header and responsive feat

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,28 @@
 const Contact = () => {
-  return <div className="contact"></div>;
+  return (
+    <div className="contact">
+      <header className="contact__header">
+        <div className="contact__header__content">
+          <h3 className="contact__header__text t-uppercase mb-1">
+            FOR INQUIRIES, COMPLIMENTS, RECRUITMENT,
+            <span>OR TO DISCUSS BUSINESS OPPORTUNITIES,</span>
+            <span>WE'D LOVE TO HEAR FROM YOU.</span>
+          </h3>
+          <h1 className="heading-1 mb-2">
+            <span className="d-block">give us</span>
+            <span className="d-block">a holler</span>
+          </h1>
+          <button className="contact__header__button mb-2">
+            Get Support Now
+          </button>
+          <h4 className="contact__header__text">
+            We are here to assist you 24/7/365
+          </h4>
+        </div>
+      </header>
+      <main className="contact__main"></main>
+    </div>
+  );
 };
 
 export default Contact;
